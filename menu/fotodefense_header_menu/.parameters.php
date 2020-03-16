a80d9041c04f4b927e094a86c1b49735ee0767fd
naval-flanker <62257806+naval-flanker@users.noreply.github.com>
2020-03-16 21:27:01 +0300

    добавлены файлы задания

---
+++ menu/fotodefense_header_menu/.parameters.php
@@ -0,0 +1,13 @@
+﻿<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateParameters["MENU_TITLE"] = Array(
+	"NAME"=>"Заголовок меню",
+	"TYPE" => "STRING",
+	"DEFAULT" => "Информация",
+	"PARENT" => "BASE",	
+);
+
+
+
+
+?>
---
+++ menu/fotodefense_top_menu/lang/en/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS ['MENU_BLUE_TABS_NAME'] = "NX Miltulevel horizontal menu";
+$MESS ['MENU_BLUE_TABS_DESC'] = "NX Miltulevel horizontal menu";
+?>
