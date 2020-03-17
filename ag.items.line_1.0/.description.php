b56511002c65d24065915fcaf085c00107a831c7
naval-flanker <62257806+naval-flanker@users.noreply.github.com>
2020-03-17 09:29:00 +0300

    Update .description.php

--- ag.items.line_1.0/.description.php
+++ ag.items.line_1.0/.description.php
@@ -4,7 +4,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 $arComponentDescription = array(
 	"NAME" => GetMessage("T_IBLOCK_DESC_LINE"),
 	"DESCRIPTION" => GetMessage("T_IBLOCK_DESC_LINE_DESC"),
-	"ICON" => "/images/news_line.gif",
 	"SORT" => 10,
 	"CACHE_PATH" => "Y",
 	"PATH" => array(
